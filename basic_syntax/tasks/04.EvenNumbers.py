4ecb8a4ff9550d16c1fcd10889e0d85df560e556
Pavel Georgiev <p.georgiev2003@gmail.com>
2023-10-05 23:35:03 +0300

    Basic syntax tasks

---
+++ basic_syntax/tasks/04.EvenNumbers.py
@@ -0,0 +1,9 @@
+n = int(input())
+
+for i in range(0,n):
+    number = int(input())
+    if number%2==1:
+        print(f"{number} is odd!")
+        break
+else:
+    print("All numbers are even.")
