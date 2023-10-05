50613b6155bcba83498c9376e2b9f2bd668532c7
Pavel Georgiev <p.georgiev2003@gmail.com>
2023-10-05 23:35:31 +0300

    Basic syntax tasks

---
+++ basic_syntax/tasks/07.Patterns.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+for i in range(1,n+1):
+    for j in range(0,i):
+        print("*",end="")
+    print()
+for i in range(n,1,-1):
+    for j in range(1,i):
+        print("*",end="")
+    print()
