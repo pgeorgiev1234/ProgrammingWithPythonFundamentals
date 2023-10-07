6aebb5a97711b9d1e4c5c625b5145761ab412a88
Pavel Georgiev <p.georgiev2003@gmail.com>
2023-10-07 21:20:47 +0300

    Data Types and Variables Tasks

---
+++ DataTypesandVariables/04.CenturiestoMinutes.py
@@ -0,0 +1,10 @@
+import math
+
+centuries = int(input())
+
+years = centuries*100
+days = math.floor(years*365.2422)
+hours = days*24
+minutes = hours*60
+
+print(f"{centuries} centuries = {years} years = {days:.0f} days = {hours:.0f} hours = {minutes:.0f} minutes")
